612bd93b4d42da6733974409fafb7f906631ca22
Anel Hadzic <77800103+AnelHadzic@users.noreply.github.com>
2023-10-28 01:21:23 +0200

    Added comment

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export function GET(request: Request) {
   try {
-    // vanligvis har vi en funksjon for å hente fra database, men nå bare returnerer jeg data fra data/ShoppingItems filen
+    // vanligvis har vi en funksjon for å hente fra database, men nå bare returnerer jeg data fra FakeDatabase/Product.ts
     const returnedData = Product;
     return NextResponse.json({ data: returnedData });
   } catch (error) {
@@ -18,6 +18,7 @@ export function GET(request: Request) {
 export async function POST(request: Request) {
   const body = await request.json();
   try {
+    // Legger til funksjon som pusher til FakeDatabase. Intern minne lagring da jeg ikke har koblet database enda :(
     addProduct(body);
     return NextResponse.json({ data: body }, { status: 201 });
   } catch (error) {
